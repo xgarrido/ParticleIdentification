db87ffc6829c1a5056d63d0dbeae16ad7764e8e8
Xavier Garrido <xavier.garrido@gmail.com>
2015-04-05 11:05:29 +0200

    refactor tof driver (to be continued)

--- source/falaise/snemo/reconstruction/tof_driver.cc
+++ source/falaise/snemo/reconstruction/tof_driver.cc
@@ -132,73 +132,61 @@ namespace snemo {
       return;
     }
 
-  int tof_driver::_process_algo(const snemo::datamodel::particle_track & particle_1_,
-                                const snemo::datamodel::particle_track & particle_2_,
-                                double & proba_int_, double & proba_ext_)
+    int tof_driver::_process_algo(const snemo::datamodel::particle_track & pt1_,
+                                  const snemo::datamodel::particle_track & pt2_,
+                                  double & proba_int_, double & proba_ext_)
     {
       DT_LOG_TRACE(get_logging_priority(), "Entering...");
-
-      const datatools::properties & aux_1 = particle_1_.get_auxiliaries();
-      const datatools::properties & aux_2 = particle_2_.get_auxiliaries();
-
-      if(!aux_1.has_key(snemo::datamodel::pid_utils::pid_label_key()) ||
-         !aux_2.has_key(snemo::datamodel::pid_utils::pid_label_key()))
-        DT_THROW_IF(true, std::logic_error,
-                    "Missing information on the particle type !");
-
-      const std::string label_1 = aux_1.fetch_string(snemo::datamodel::pid_utils::pid_label_key());
-      const std::string label_2 = aux_2.fetch_string(snemo::datamodel::pid_utils::pid_label_key());
 
       // C'est pas du C, c'est juste plus propre
       double E1, E2;
       double track_length_1, track_length_2;
       double t1, t2, sigma_t1, sigma_t2;
 
-      double m1 = _get_mass(particle_1_) * CLHEP::MeV;
-      double m2 = _get_mass(particle_2_) * CLHEP::MeV;
+      const double m1 = _get_mass(pt1_);
+      const double m2 = _get_mass(pt2_);
 
       //either specialize the methods or consider the case here
 
-      if(label_1 == snemo::datamodel::pid_utils::electron_label() &&
-         label_2 == snemo::datamodel::pid_utils::electron_label())
-        {
-          E1 = _get_energy(particle_1_);
-          E2 = _get_energy(particle_2_);
+      if (snemo::datamodel::pid_utils::particle_is_electron(pt1_) &&
+          snemo::datamodel::pid_utils::particle_is_electron(pt2_)) {
+        E1 = _get_energy(pt1_);
+        E2 = _get_energy(pt2_);
 
-          _get_times(particle_1_, t1, sigma_t1);
-          _get_times(particle_2_, t2, sigma_t2);
+        _get_times(pt1_, t1, sigma_t1);
+        _get_times(pt2_, t2, sigma_t2);
 
-          _get_track_length(particle_1_, particle_2_, track_length_1, track_length_2);
-        }
+        _get_track_length(pt1_, pt2_, track_length_1, track_length_2);
 
-      //should take care in the topology driver not to feed two gamma
-      if(label_1 == snemo::datamodel::pid_utils::gamma_label() ||
-         label_2 == snemo::datamodel::pid_utils::gamma_label())
-        {
-          /* /!\ For now, suppose the gamma */
+      } else if (snemo::datamodel::pid_utils::particle_is_gamma(pt1_) &&
+                 snemo::datamodel::pid_utils::particle_is_gamma(pt2_)) {
+        // should take care in the topology driver not to feed two gamma
+        /* /!\ For now, suppose the gamma */
 
-          E1 = 1.; // not relevant for gamma since beta=1
-          E2 = 1.; // but still, not 0 because involved in fraction
+        E1 = 1.; // not relevant for gamma since beta=1
+        E2 = 1.; // but still, not 0 because involved in fraction
 
-          double t1, t2, sigma_t1, sigma_t2;
-          _get_times(particle_1_, t1, sigma_t1);
-          _get_times(particle_2_, t2, sigma_t2);
+        _get_times(pt1_, t1, sigma_t1);
+        _get_times(pt2_, t2, sigma_t2);
 
-          _get_track_length(particle_1_, particle_2_, track_length_1, track_length_2);
+        _get_track_length(pt1_, pt2_, track_length_1, track_length_2);
 
-        }
+      } else {
+        DT_LOG_WARNING(get_logging_priority(), "Topology not supported !");
+        return 1;
+      }
 
       double t1_first, t1_last, t2_first, t2_last;
       double sigma_t1_first, sigma_t1_last, sigma_t2_first, sigma_t2_last;
 
-      _get_times(particle_1_, t1_first, sigma_t1_first, t1_last, sigma_t1_last);
-      _get_times(particle_2_, t2_first, sigma_t2_first, t2_last, sigma_t2_last);
+      _get_times(pt1_, t1_first, sigma_t1_first, t1_last, sigma_t1_last);
+      _get_times(pt2_, t2_first, sigma_t2_first, t2_last, sigma_t2_last);
 
       std::cout << "E1 : " <<  E1 << std::endl
                 << "E2 : " <<  E2 << std::endl;
 
 
-      _get_track_length(particle_1_, particle_2_, track_length_1, track_length_2);
+      _get_track_length(pt1_, pt2_, track_length_1, track_length_2);
 
       std::cout << "track length 1 : " <<  track_length_1 << std::endl
                 << "track length 2 : " <<  track_length_2 << std::endl
@@ -323,27 +311,22 @@ namespace snemo {
     {
       DT_LOG_TRACE(get_logging_priority(), "Entering...");
 
-      const datatools::properties & aux = particle_.get_auxiliaries();
+      double mass = datatools::invalid_real();
 
-      /* probably a cleaner way to do it*/
-      if(!aux.has_key(snemo::datamodel::pid_utils::pid_label_key()))
-        DT_THROW_IF(true, std::logic_error,
-                    "Missing information on the particle type !");
-
-      const std::string label = aux.fetch_string(snemo::datamodel::pid_utils::pid_label_key());
-
-#warning get mass from CLHEP
-      if(label == snemo::datamodel::pid_utils::electron_label())
-        return 0.511;
-      else if(label == snemo::datamodel::pid_utils::gamma_label())
-        return 0;
-      else
+      if (snemo::datamodel::pid_utils::particle_is_electron(particle_) ||
+          snemo::datamodel::pid_utils::particle_is_positron(particle_)) {
+        mass = CLHEP::electron_mass_c2;
+      } else if (snemo::datamodel::pid_utils::particle_is_gamma(particle_)) {
+        mass = 0.0 * CLHEP::eV;
+      } else if (snemo::datamodel::pid_utils::particle_is_alpha(particle_)) {
+        mass = 3.727417 * CLHEP::GeV;
+      } else {
         DT_THROW_IF(true, std::logic_error,
                     "Particle type inappropriate for TOF calculations !");
+      }
 
       DT_LOG_TRACE(get_logging_priority(), "Exiting...");
-
-      return 0;
+      return mass;
     }
 
     int tof_driver::_get_track_length(const snemo::datamodel::particle_track & pt1_,
@@ -453,48 +436,48 @@ namespace snemo {
       return 0; // for now
     }
 
-//     double tof_driver::_get_tof_probability(const snemo::datamodel::calibrated_calorimeter_hit & head_end_calo_hit_,
-//                                                          const snemo::datamodel::calibrated_calorimeter_hit & tail_begin_calo_hit_) const
-//     {
-//       const snemo::geometry::calo_locator & calo_locator   = base_gamma_builder::get_calo_locator();
-//       const snemo::geometry::xcalo_locator & xcalo_locator = base_gamma_builder::get_xcalo_locator();
-//       const snemo::geometry::gveto_locator & gveto_locator = base_gamma_builder::get_gveto_locator();
+    //     double tof_driver::_get_tof_probability(const snemo::datamodel::calibrated_calorimeter_hit & head_end_calo_hit_,
+    //                                                          const snemo::datamodel::calibrated_calorimeter_hit & tail_begin_calo_hit_) const
+    //     {
+    //       const snemo::geometry::calo_locator & calo_locator   = base_gamma_builder::get_calo_locator();
+    //       const snemo::geometry::xcalo_locator & xcalo_locator = base_gamma_builder::get_xcalo_locator();
+    //       const snemo::geometry::gveto_locator & gveto_locator = base_gamma_builder::get_gveto_locator();
 
-//       geomtools::vector_3d head_position;
-//       const geomtools::geom_id & head_gid = head_end_calo_hit_.get_geom_id();
-//       if (calo_locator.is_calo_block_in_current_module(head_gid))
-//         calo_locator.get_block_position(head_gid, head_position);
-//       else if (xcalo_locator.is_calo_block_in_current_module(head_gid))
-//         xcalo_locator.get_block_position(head_gid, head_position);
-//       else if (gveto_locator.is_calo_block_in_current_module(head_gid))
-//         gveto_locator.get_block_position(head_gid, head_position);
-//       else
-//         DT_THROW_IF(true, std::logic_error,
-//                     "Current geom id '" << head_gid << "' does not match any scintillator block !");
+    //       geomtools::vector_3d head_position;
+    //       const geomtools::geom_id & head_gid = head_end_calo_hit_.get_geom_id();
+    //       if (calo_locator.is_calo_block_in_current_module(head_gid))
+    //         calo_locator.get_block_position(head_gid, head_position);
+    //       else if (xcalo_locator.is_calo_block_in_current_module(head_gid))
+    //         xcalo_locator.get_block_position(head_gid, head_position);
+    //       else if (gveto_locator.is_calo_block_in_current_module(head_gid))
+    //         gveto_locator.get_block_position(head_gid, head_position);
+    //       else
+    //         DT_THROW_IF(true, std::logic_error,
+    //                     "Current geom id '" << head_gid << "' does not match any scintillator block !");
 
-//       geomtools::vector_3d tail_position;
-//       const geomtools::geom_id & tail_gid = tail_begin_calo_hit_.get_geom_id();
-//       if (calo_locator.is_calo_block_in_current_module(tail_gid))
-//         calo_locator.get_block_position(tail_gid, tail_position);
-//       else if (xcalo_locator.is_calo_block_in_current_module(tail_gid))
-//         xcalo_locator.get_block_position(tail_gid, tail_position);
-//       else if (gveto_locator.is_calo_block_in_current_module(tail_gid))
-//         gveto_locator.get_block_position(tail_gid, tail_position);
-//       else
-//         DT_THROW_IF(true, std::logic_error,
-//                     "Current geom id '" << tail_gid << "' does not match any scintillator block !");
+    //       geomtools::vector_3d tail_position;
+    //       const geomtools::geom_id & tail_gid = tail_begin_calo_hit_.get_geom_id();
+    //       if (calo_locator.is_calo_block_in_current_module(tail_gid))
+    //         calo_locator.get_block_position(tail_gid, tail_position);
+    //       else if (xcalo_locator.is_calo_block_in_current_module(tail_gid))
+    //         xcalo_locator.get_block_position(tail_gid, tail_position);
+    //       else if (gveto_locator.is_calo_block_in_current_module(tail_gid))
+    //         gveto_locator.get_block_position(tail_gid, tail_position);
+    //       else
+    //         DT_THROW_IF(true, std::logic_error,
+    //                     "Current geom id '" << tail_gid << "' does not match any scintillator block !");
 
-//       const double t1 = head_end_calo_hit_.get_time();
-//       const double t2 = tail_begin_calo_hit_.get_time();
-//       const double track_length = (head_position-tail_position).mag();
-//       const double t_th = track_length / CLHEP::c_light;
-// #warning sigma_l unit must be fixed !
-//       const double sigma_l = 0.6 * CLHEP::ns;
-//       const double sigma_exp = pow(head_end_calo_hit_.get_sigma_time(),2)
-//         + pow(tail_begin_calo_hit_.get_sigma_time(),2) + pow(sigma_l,2);
-//       const double chi2 = pow(std::abs(t1 - t2) - t_th,2)/sigma_exp;
-//       return gsl_cdf_chisq_Q(chi2, 1);
-//     }
+    //       const double t1 = head_end_calo_hit_.get_time();
+    //       const double t2 = tail_begin_calo_hit_.get_time();
+    //       const double track_length = (head_position-tail_position).mag();
+    //       const double t_th = track_length / CLHEP::c_light;
+    // #warning sigma_l unit must be fixed !
+    //       const double sigma_l = 0.6 * CLHEP::ns;
+    //       const double sigma_exp = pow(head_end_calo_hit_.get_sigma_time(),2)
+    //         + pow(tail_begin_calo_hit_.get_sigma_time(),2) + pow(sigma_l,2);
+    //       const double chi2 = pow(std::abs(t1 - t2) - t_th,2)/sigma_exp;
+    //       return gsl_cdf_chisq_Q(chi2, 1);
+    //     }
 
   } // end of namespace reconstruction
 
