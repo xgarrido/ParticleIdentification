62c00d7bee1631e613280c0c3aedf61ee3fbf6b2
Xavier Garrido <xavier.garrido@gmail.com>
2015-04-15 18:12:18 +0200

    PID cut moved from topology cut into a dedicated i_cut class

---
+++ source/falaise/snemo/cuts/pid_cut.cc
@@ -0,0 +1,178 @@
+// falaise/snemo/cuts/pid_cut.cc
+
+// Ourselves:
+#include <falaise/snemo/cuts/pid_cut.h>
+
+// Standard library:
+#include <stdexcept>
+#include <sstream>
+
+// Third party:
+// - Bayeux/datatools:
+#include <datatools/properties.h>
+#include <datatools/things.h>
+
+// SuperNEMO data models :
+#include <falaise/snemo/datamodels/pid_utils.h>
+#include <falaise/snemo/datamodels/data_model.h>
+#include <falaise/snemo/datamodels/particle_track_data.h>
+
+namespace snemo {
+
+  namespace cut {
+
+    // Registration instantiation macro :
+    CUT_REGISTRATION_IMPLEMENT(pid_cut, "snemo::cut::pid_cut");
+
+    pid_cut::particle_range::particle_range()
+    {
+      min = 0;
+      max = 0;
+      return;
+    }
+
+    void pid_cut::particle_range::parse(const datatools::properties & setup_,
+                                        const std::string & prefix_)
+    {
+      if (setup_.has_key(prefix_ + "_range.min")) {
+        min = setup_.fetch_integer(prefix_ + "_range.min");
+      }
+      if (setup_.has_key(prefix_ + "_range.max")) {
+        max = setup_.fetch_integer(prefix_ + "_range.max");
+      }
+      return;
+    }
+
+    bool pid_cut::particle_range::check(const size_t n_)
+    {
+      if (n_ < min) {
+        return false;
+      }
+      if (n_ > max) {
+        return false;
+      }
+      return true;
+    }
+
+    void pid_cut::_set_defaults()
+    {
+      _PTD_label_ = snemo::datamodel::data_info::default_particle_track_data_label();
+      return;
+    }
+
+    pid_cut::pid_cut(datatools::logger::priority logger_priority_)
+      : cuts::i_cut(logger_priority_)
+    {
+      _set_defaults();
+      return;
+    }
+
+    pid_cut::~pid_cut()
+    {
+      if (is_initialized()) this->pid_cut::reset();
+      return;
+    }
+
+    void pid_cut::reset()
+    {
+      _set_defaults();
+      this->i_cut::_reset();
+      this->i_cut::_set_initialized(false);
+      return;
+    }
+
+    void pid_cut::initialize(const datatools::properties & configuration_,
+                             datatools::service_manager  & /* service_manager_ */,
+                             cuts::cut_handle_dict_type  & /* cut_dict_ */)
+    {
+      DT_THROW_IF(is_initialized(), std::logic_error,
+                  "Cut '" << get_name() << "' is already initialized ! ");
+
+      this->i_cut::_common_initialize(configuration_);
+
+      if (configuration_.has_key("PTD_label")) {
+        _PTD_label_ = configuration_.fetch_string("PTD_label");
+      }
+
+      _electron_range_.parse(configuration_, "electron");
+      _positron_range_.parse(configuration_, "positron");
+      _gamma_range_.parse(configuration_, "gamma");
+      _alpha_range_.parse(configuration_, "alpha");
+      _undefined_range_.parse(configuration_, "undefined");
+
+      this->i_cut::_set_initialized(true);
+      return;
+    }
+
+
+    int pid_cut::_accept()
+    {
+      uint32_t cut_returned = cuts::SELECTION_INAPPLICABLE;
+
+      // Get event record
+      const datatools::things & ER = get_user_data<datatools::things>();
+
+      if (! ER.has(_PTD_label_)) {
+        DT_LOG_DEBUG(get_logging_priority(), "Event record has no '" << _PTD_label_ << "' bank !");
+        return cut_returned;
+      }
+      const snemo::datamodel::particle_track_data & PTD
+        = ER.get<snemo::datamodel::particle_track_data>(_PTD_label_);
+
+      const datatools::properties & aux = PTD.get_auxiliaries();
+
+      size_t nelectrons = 0;
+      size_t npositrons = 0;
+      size_t nalphas    = 0;
+      size_t ngammas    = 0;
+      size_t nundefined = 0;
+
+      std::string key;
+      if (aux.has_key(key = snemo::datamodel::pid_utils::electron_label())) {
+        nelectrons = aux.fetch_integer(key);
+      }
+      if (aux.has_key(key = snemo::datamodel::pid_utils::positron_label())) {
+        npositrons = aux.fetch_integer(key);
+      }
+      if (aux.has_key(key = snemo::datamodel::pid_utils::gamma_label())) {
+        ngammas = aux.fetch_integer(key);
+      }
+      if (aux.has_key(key = snemo::datamodel::pid_utils::alpha_label())) {
+        nalphas = aux.fetch_integer(key);
+      }
+      if (aux.has_key(key = snemo::datamodel::pid_utils::undefined_label())) {
+        nundefined = aux.fetch_integer(key);
+      }
+
+      DT_LOG_TRACE(get_logging_priority(), "nelectron  = " << nelectrons);
+      DT_LOG_TRACE(get_logging_priority(), "npositron  = " << npositrons);
+      DT_LOG_TRACE(get_logging_priority(), "nalphas    = " << nalphas);
+      DT_LOG_TRACE(get_logging_priority(), "ngammas    = " << ngammas);
+      DT_LOG_TRACE(get_logging_priority(), "nundefined = " << nundefined);
+
+      bool check = true;
+      if (! _electron_range_.check(nelectrons)) check = false;
+      if (! _positron_range_.check(npositrons)) check = false;
+      if (! _gamma_range_.check(ngammas)) check = false;
+      if (! _alpha_range_.check(nalphas)) check = false;
+      if (! _undefined_range_.check(nundefined)) check = false;
+
+      cut_returned = cuts::SELECTION_ACCEPTED;
+      if (! check) {
+        DT_LOG_DEBUG(get_logging_priority(), "Event rejected by pid cut!");
+        cut_returned = cuts::SELECTION_REJECTED;
+      }
+
+      return cut_returned;
+    }
+
+  }  // end of namespace cut
+
+}  // end of namespace snemo
+
+/*
+** Local Variables: --
+** mode: c++ --
+** c-file-style: "gnu" --
+** End: --
+*/
---
+++ source/falaise/snemo/cuts/pid_cut.h
@@ -0,0 +1,114 @@
+/// \file falaise/snemo/cuts/pid_cut.h
+/* Author(s)     : Xavier Garrido <garrido@lal.in2p3.fr>
+ * Creation date : 2015-05-15
+ * Last modified : 2015-05-15
+ *
+ * Copyright (C) 2015 Xavier Garrido <garrido@lal.in2p3.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Description:
+ *
+ *   PID event cut.
+ *
+ * History:
+ *
+ */
+
+#ifndef FALAISE_SNEMO_CUT_PID_CUT_H
+#define FALAISE_SNEMO_CUT_PID_CUT_H 1
+
+// Standard library:
+#include <string>
+
+// Third party:
+// - Boost:
+#include <boost/cstdint.hpp>
+// - Bayeux/cuts:
+#include <cuts/i_cut.h>
+
+namespace snemo {
+
+  namespace cut {
+
+    /// \brief A pid event cut
+    class pid_cut : public cuts::i_cut
+    {
+    public:
+
+      /// Structure holding particle range
+      struct particle_range {
+        size_t min;
+        size_t max;
+
+        /// Constructor
+        particle_range();
+
+        /// Parse min/max value from configuration
+        void parse(const datatools::properties & setup_, const std::string & prefix_);
+
+        /// Check number of particle
+        bool check(const size_t n_);
+      };
+
+      /// Constructor
+      pid_cut(datatools::logger::priority logging_priority_ = datatools::logger::PRIO_FATAL);
+
+      /// Destructor
+      virtual ~pid_cut();
+
+      /// Initilization
+      virtual void initialize(const datatools::properties & configuration_,
+                              datatools::service_manager & service_manager_,
+                              cuts::cut_handle_dict_type & cut_dict_);
+
+      /// Reset
+      virtual void reset();
+
+    protected:
+
+      /// Default values
+      void _set_defaults();
+
+      /// Selection
+      virtual int _accept();
+
+    private:
+
+      std::string _PTD_label_; //!< Name of the "Particle track data" bank
+
+      particle_range _electron_range_; //!< Number of electrons
+      particle_range _positron_range_; //!< Number of positrons
+      particle_range _gamma_range_;    //!< Number of gammas
+      particle_range _alpha_range_;    //!< Number of alphas
+      particle_range _undefined_range_;//!< Number of undefined particles
+
+      // Macro to automate the registration of the cut :
+      CUT_REGISTRATION_INTERFACE(pid_cut);
+    };
+
+  }  // end of namespace cut
+
+}  // end of namespace snemo
+
+#endif // FALAISE_SNEMO_CUT_PID_CUT_H
+
+/*
+** Local Variables: --
+** mode: c++ --
+** c-file-style: "gnu" --
+** End: --
+*/
