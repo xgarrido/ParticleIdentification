81b9d2991fbb3bbba0b1cf8dc1763f2450a9d0eb
Xavier Garrido <xavier.garrido@gmail.com>
2015-04-15 20:49:48 +0200

    fix 1e topology serialization

--- source/falaise/snemo/datamodels/the_serializable_bis.h
+++ source/falaise/snemo/datamodels/the_serializable_bis.h
@@ -18,20 +18,31 @@ DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(snemo::datamodel::TOF_me
 BOOST_CLASS_EXPORT_IMPLEMENT(snemo::datamodel::TOF_measurement)
 
 
-/***********************************
- * snemo::datamodel::topology_data *
- ***********************************/
+/**************************************
+ * snemo::datamodel::topology_pattern *
+ **************************************/
 
 #include <falaise/snemo/datamodels/base_topology_pattern.ipp>
+
+#include <falaise/snemo/datamodels/topology_1e_pattern.ipp>
+DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(snemo::datamodel::topology_1e_pattern)
+BOOST_CLASS_EXPORT_IMPLEMENT(snemo::datamodel::topology_1e_pattern)
+
 #include <falaise/snemo/datamodels/topology_2e_pattern.ipp>
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(snemo::datamodel::topology_2e_pattern)
 BOOST_CLASS_EXPORT_IMPLEMENT(snemo::datamodel::topology_2e_pattern)
+
 #include <falaise/snemo/datamodels/topology_2eNg_pattern.ipp>
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(snemo::datamodel::topology_2eNg_pattern)
 BOOST_CLASS_EXPORT_IMPLEMENT(snemo::datamodel::topology_2eNg_pattern)
+
 #include <falaise/snemo/datamodels/topology_1e1g_pattern.ipp>
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(snemo::datamodel::topology_1e1g_pattern)
 BOOST_CLASS_EXPORT_IMPLEMENT(snemo::datamodel::topology_1e1g_pattern)
+
+/***********************************
+ * snemo::datamodel::topology_data *
+ ***********************************/
 
 #include <falaise/snemo/datamodels/topology_data.ipp>
 DATATOOLS_SERIALIZATION_CLASS_SERIALIZE_INSTANTIATE_ALL(snemo::datamodel::topology_data)
